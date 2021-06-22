91b4b204da1c01860169388d9a8beda457d227bb
EC2 Default User <ec2-user@ip-172-31-22-162.ap-northeast-1.compute.internal>
2021-06-22 12:32:24 +0000

    first commit

--- MyTasks/config/application.rb
+++ MyTasks/config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module MyTasks
+module TasksForMe
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
@@ -15,5 +15,13 @@ module MyTasks
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    
+    #日時を日本時間に変換
+    config.time_zone = 'Tokyo'
+    
+    # ヴァリデーションを日本語化
+    config.i18n.default_locale = :ja
+    config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}').to_s]
+    
   end
 end
